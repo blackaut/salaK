78f143c74c948387189dd358d35796715868e8b1
Fabian <fabian@fabz.tv>
2016-12-23 18:57:54 -0300

    details

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -417,15 +417,47 @@
 
 			FBZ.model.centralContainer = ""; 
 			// Object.keys(FBZ.model.noBrain.cartelera.elements[0]).forEach(function(key,index) {
-    		for ( var i = 0 ; i < FBZ.model.noBrain.nosotros.elements.length ; i ++ ) { 
 //				
 				// console.log(FBZ.model.noBrain.cartelera.elements[i]);
 				
     				// key: the name of the object key
 				// console.log(key,index);
-					FBZ.model.centralContainer += "<iframe src='http://eepurl.com/bTgx0b'></iframe>";
+			FBZ.model.centralContainer += 
 
-				}
+				"<link href='//cdn-images.mailchimp.com/embedcode/classic-10_7.css' rel='stylesheet' type='text/css'>"+
+				"<style type='text/css'>"+
+				"#mc_embed_signup { background:transparent; clear:left; }"+
+				"</style>"+
+				"<div id='mc_embed_signup'>"+
+				"<form action='//k-i.us13.list-manage.com/subscribe/post?u=61e392528256ac7a8e5ea3ac3&amp;id=1eb7ae6282' method='post' id='mc-embedded-subscribe-form' name='mc-embedded-subscribe-form' class='validate' target='_blank' novalidate>"+
+				"<div id='mc_embed_signup_scroll'>"+
+				"<h2>Incribete en nuestra lista de mail</h2>"+
+				"<div class='indicates-required'><span class='asterisk'>*</span> indicates required</div>"+
+				"<div class='mc-field-group'>"+
+					"<label for='mce-EMAIL'>Correo Electronico<span class='asterisk'>*</span>"+
+				"</label>"+
+					"<input type='email' value='' name='EMAIL' class='required email' id='mce-EMAIL'>"+
+				"</div>"+
+				"<div class='mc-field-group'>"+
+					"<label for='mce-FNAME'>Nombre </label>"+
+					"<input type='text' value=' name='FNAME' class=' id='mce-FNAME'>"+
+				"</div>"+
+				"<div class='mc-field-group'>"+
+					"<label for='mce-LNAME'>Apellido  <span class='asterisk'>*</span>"+
+				"</label>"+
+					"<input type='text' value=' name='LNAME' class='required' id='mce-LNAME'>"+
+				"</div>"+
+					"<div id='mce-responses' class='clear'>"+
+						"<div class='response' id='mce-error-response' style='display:none'></div>"+
+						"<div class='response' id='mce-success-response' style='display:none'></div>"+
+					"</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->"+
+				"<div style='position: absolute; left: -5000px;' aria-hidden='true'><input type='text' name='b_61e392528256ac7a8e5ea3ac3_1eb7ae6282' tabindex='-1' value='></div>"+
+					"<div class='clear'><input type='submit' value='Subscribe' name='subscribe' id='mc-embedded-subscribe' class='button'></div>"+
+				"</div>"+
+				"</form>"+
+				"</div>"+
+				"<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>";
+				// <!--End mc_embed_signup-->
 
 			FBZ.view.$centralContainer.append(FBZ.model.centralContainer);
 
